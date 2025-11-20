8cbf44de6f7039ea48e8ffd74f949ffe402bce32
ayyappareddy007 <ayyappareddydwarampudi007@gmail.com>
2025-11-20 22:20:26 +0530

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -71,6 +71,24 @@ export class Appliances extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json()
+  }).then((productData) => {
+    products = productData.map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      else if(productDetails.type === 'appliance') {
+        return new Appliances(productDetails);
+      }
+      return new Product(productDetails);
+    });
+  });
+  return promise;
+}
+// loadProductsFetch()
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
