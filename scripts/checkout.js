d603d8d966ab38636693e04109476a15d97ce670
ayyappareddy007 <ayyappareddydwarampudi007@gmail.com>
2025-11-22 18:34:13 +0530

    pratice fetch, promise, async and await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,17 +2,15 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts,loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js"; 
+import { loadCart, loadCartFetch } from "../data/cart.js"; 
 
 async function loadPage() {
     try {
-        await loadProductsFetch();
-
-        await new Promise((resolve) => {
-            loadCart(() => {
-                resolve();
-            });
-        });
+        
+        await Promise.all([
+            loadProductsFetch(),
+            loadCartFetch()
+        ]);
 
     } catch(error) {
         console.log('Unexpected error. Please try again later.');
